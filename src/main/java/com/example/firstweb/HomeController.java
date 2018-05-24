87b0d6aae961af209b6b4531d04a5043ed622c9b
heshichao1995 <m18522603436_1@163.com>
2018-05-24 12:48:06 +0800

    firstweb

--- src/main/java/com/example/firstweb/HomeController.java
+++ src/main/java/com/example/firstweb/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/")
 	public String index() {
-		return "Hello, 何世超!";
+		return " 何世超!";
 	}
 }
